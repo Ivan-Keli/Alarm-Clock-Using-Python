203793acfe41c366292a3734852b2a63c1c62759
Ivan-Keli <ivankeli174@gmail.com>
2024-04-02 22:38:43 +0300

    first commit

---
+++ play_sound.py
@@ -0,0 +1,8 @@
+# Running this code will play the sound
+
+# Here is the sound effect that i downloaded to be used as my alarm.
+# https://www.fesliyanstudios.com/play-mp3/4384
+
+from playsound import playsound
+
+playsound("Alarm-Fast-High-Pitch-A1.mp3")
